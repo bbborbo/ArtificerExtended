cd943980248b4363b51094ccea73e9871e928ef9
BORBO <bwofodol@gmail.com>
2023-11-06 00:34:39 -0600

    forgot some things for heat + ice lol

--- AltArtificerExtended/CoreModules/SkillHooks.cs
+++ AltArtificerExtended/CoreModules/SkillHooks.cs
@@ -22,6 +22,7 @@ using ArtificerExtended;
 using ArtificerExtended.Components;
 using static ArtificerExtended.Passive.AltArtiPassive;
 using static ArtificerExtended.Components.ElementCounter;
+using static ChillRework.ChillRework;
 
 namespace ArtificerExtended
 {
@@ -53,7 +54,9 @@ namespace ArtificerExtended
             //On.RoR2.CharacterBody.RecalculateStats += CharacterBody_RecalculateStats;
             //On.RoR2.CharacterBody.AddBuff_BuffIndex += CharacterBody_AddBuff_BuffIndex;
             On.RoR2.CharacterMaster.OnBodyStart += CharacterMaster_OnBodyStart;
+            OnMaxChill += FrostNovaOnMaxChill;
         }
+
 
         private void CharacterMaster_OnBodyStart(On.RoR2.CharacterMaster.orig_OnBodyStart orig, CharacterMaster self, CharacterBody body)
         {
@@ -153,63 +156,23 @@ namespace ArtificerExtended
                     }
                 }
             }
-            CharacterBody vBody = self.body;
+        }
+
+        private void FrostNovaOnMaxChill(DamageInfo damageInfo, GameObject victimObject)
+        {
+            CharacterBody vBody = victimObject.GetComponent<CharacterBody>();
             CharacterBody aBody = null;
             if (damageInfo.attacker != null)
                 aBody = damageInfo.attacker.GetComponent<CharacterBody>();
-
-            if (vBody != null && aBody != null && damageInfo.procCoefficient != 0 && !damageInfo.rejected)
+            if(aBody != null && vBody != null)
             {
                 Power icePower = GetIcePowerLevelFromBody(aBody);
-                float debuffDuration = AltArtiPassive.chillProcDuration;
-
-                if (damageInfo.damageType.HasFlag(DamageType.Freeze2s))
-                {
-                    float chillCount = AltArtiPassive.freezeProcCount;
-                    if (damageInfo.damageType.HasFlag(DamageType.AOE))
-                    {
-                        chillCount -= 1;
-                    }
-                    for (int i = 0; i < chillCount; i++)
-                    {
-                        if (Util.CheckRoll(damageInfo.procCoefficient * 100, aBody.master))
-                        {
-                            vBody.AddTimedBuffAuthority(RoR2Content.Buffs.Slow80.buffIndex, debuffDuration);
-                        }
-                    }
-                }
-                else if (aBody.bodyIndex == BodyCatalog.FindBodyIndex(ArtificerExtendedPlugin.mageBody))
-                {
-                    if (damageInfo.damageType.HasFlag(DamageType.SlowOnHit))
-                    {
-                        damageInfo.damageType = damageInfo.damageType & ~DamageType.SlowOnHit;
-                        float procChance = Mathf.Min(1, AltArtiPassive.slowProcChance * damageInfo.procCoefficient * damageInfo.procCoefficient) * 100;
-
-                        if (Util.CheckRoll(procChance, aBody.master))
-                        {
-                            vBody.AddTimedBuffAuthority(RoR2Content.Buffs.Slow80.buffIndex, debuffDuration);
-                        }
-                    }
-                }
-
-
-                int chillDebuffCount = vBody.GetBuffCount(RoR2Content.Buffs.Slow80);
-                if (chillDebuffCount >= AltArtiPassive.novaDebuffThreshold && icePower > Power.None) //Arctic Blast
+                if (icePower > Power.None) //Arctic Blast
                 {
                     vBody.ClearTimedBuffs(RoR2Content.Buffs.Slow80);
                     AltArtiPassive.DoNova(aBody, icePower, damageInfo.position, AltArtiPassive.novaDebuffThreshold);
                 }
             }
-            orig(self, damageInfo);
-        }
-
-        private void CharacterBody_AddBuff_BuffIndex(On.RoR2.CharacterBody.orig_AddBuff_BuffIndex orig, CharacterBody self, BuffIndex buffType)
-        {
-            if (buffType == RoR2Content.Buffs.Slow80.buffIndex && self.GetBuffCount(RoR2Content.Buffs.Slow80.buffIndex) >= AltArtiPassive.novaDebuffThreshold)
-            {
-                return;
-            }
-            orig(self, buffType);
         }
 
         #endregion
