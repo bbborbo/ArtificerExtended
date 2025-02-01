f186e3f87cff84f4f285e31f76b40e3de9fbf358
BORBO <bwofodol@gmail.com>
2025-02-01 16:48:13 -0600

    version

--- AltArtificerExtended/ArtificerExtendedPlugin.cs
+++ AltArtificerExtended/ArtificerExtendedPlugin.cs
@@ -57,7 +57,7 @@ namespace ArtificerExtended
         public const string guid = "com.Borbo.ArtificerExtended";
         public const string modName = "ArtificerExtended";
 
-        public const string version = "3.7.11";
+        public const string version = "3.8.0";
         
         public static AssetBundle iconBundle = Tools.LoadAssetBundle(Properties.Resources.artiskillicons);
         public static string iconsPath = "Assets/AESkillIcons/";
