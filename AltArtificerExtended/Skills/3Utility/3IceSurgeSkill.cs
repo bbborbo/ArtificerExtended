c2045960672ee3bd3c8d12197bd54b75ebc38c33
BORBO <bwofodol@gmail.com>
2025-02-03 21:02:43 -0600

    created ice surge skillbase

---
+++ AltArtificerExtended/Skills/3Utility/3IceSurgeSkill.cs
@@ -0,0 +1,40 @@
+﻿using BepInEx.Configuration;
+using RoR2;
+using RoR2.Skills;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArtificerExtended.Skills
+{
+    class _3IceSurgeSkill : SkillBase
+    {
+        public override string SkillName => "Avalanche";
+
+        public override string SkillDescription => "Freezing. Burst forward, leaving an icicle at your feet for X% damage.";
+
+        public override string SkillLangTokenName => "Arctic Surge";
+
+        public override UnlockableDef UnlockDef => null;
+
+        public override string IconName => throw new NotImplementedException();
+
+        public override MageElement Element => throw new NotImplementedException();
+
+        public override Type ActivationState => throw new NotImplementedException();
+
+        public override SkillFamily SkillSlot => throw new NotImplementedException();
+
+        public override SimpleSkillData SkillData => throw new NotImplementedException();
+
+        public override void Hooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Init(ConfigFile config)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
