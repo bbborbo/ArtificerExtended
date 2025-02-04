e8fecbdded94ae718b756d80a32eb32c86c296eb
BORBO <bwofodol@gmail.com>
2025-02-04 01:44:55 -0600

    thunderstrike

---
+++ AltArtificerExtended/EntityStates/2Secondary/ThunderStrikeDash.cs
@@ -0,0 +1,270 @@
+﻿using ArtificerExtended.Passive;
+using ArtificerExtended.Skills;
+using EntityStates;
+using EntityStates.Huntress;
+using RoR2;
+using RoR2.Networking;
+using RoR2.Orbs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace ArtificerExtended.States
+{
+	class ThunderStrikeDash : BasicMeleeAttack
+	{
+		bool hasIncreasedDuration = false;
+		public float speedCoefficientOnExit;
+		private CharacterModel characterModel;
+		public float speedCoefficient => _2ThunderstrikeSkill.speedCoefficient;
+		public string endSoundString;
+		public float exitSmallHop;
+		public float delayedDamageCoefficient => _2ThunderstrikeSkill.delayDamageCoefficient;
+		public float delayedProcCoefficient => _2ThunderstrikeSkill.delayProcCoefficient;
+		public float delay = 0.25f;
+		public float delayPerHit = 0f;
+		public string enterAnimationLayerName = "Gesture, Additive";
+		public string enterAnimationStateName = "PrepWall";
+		public float enterAnimationCrossfadeDuration = 0.1f;
+		public string exitAnimationLayerName = "Gesture, Additive";
+		public string exitAnimationStateName = "FireNovaBomb";
+
+		public GameObject delayedEffectPrefab => _2ThunderstrikeSkill.lightningImpactEffect;
+		public GameObject orbEffect => _2ThunderstrikeSkill.lightningOrbEffect;
+
+		public GameObject selfOnHitOverlayEffectPrefab;
+		private Transform modelTransform;
+		private Vector3 dashVector;
+		private int originalLayer;
+		private int currentHitCount;
+		private Vector3 dashVelocity
+		{
+			get
+			{
+				return this.dashVector * this.moveSpeedStat * this.speedCoefficient * this.attackSpeedStat;
+			}
+		}
+
+        public override string GetHitBoxGroupName()
+        {
+			return ArtificerExtendedPlugin.ThunderSurgeHitBoxGroupName;
+
+		}
+        public override void OnEnter()
+		{
+			this.ignoreAttackSpeed = true;
+			this.baseDuration = _2ThunderstrikeSkill.baseDuration;
+			base.OnEnter();
+			this.dashVector = base.inputBank.aimDirection;
+			this.originalLayer = base.gameObject.layer;
+			base.gameObject.layer = LayerIndex.GetAppropriateFakeLayerForTeam(base.teamComponent.teamIndex).intVal;
+			base.characterMotor.Motor.RebuildCollidableLayers();
+			base.characterMotor.Motor.ForceUnground(0.1f);
+			base.characterMotor.velocity = Vector3.zero;
+			this.modelTransform = base.GetModelTransform();
+			if (this.modelTransform)
+			{
+				TemporaryOverlayInstance temporaryOverlay = TemporaryOverlayManager.AddOverlay(this.modelTransform.gameObject);
+				temporaryOverlay.duration = 0.6f;
+				temporaryOverlay.animateShaderAlpha = true;
+				temporaryOverlay.alphaCurve = AnimationCurve.EaseInOut(0f, 1f, 1f, 0f);
+				temporaryOverlay.destroyComponentOnEnd = true;
+				temporaryOverlay.originalMaterial = RoR2.LegacyResourcesAPI.Load<Material>("Materials/matHuntressFlashBright");
+				temporaryOverlay.AddToCharacterModel(this.modelTransform.GetComponent<CharacterModel>());
+				TemporaryOverlayInstance temporaryOverlay2 = TemporaryOverlayManager.AddOverlay(this.modelTransform.gameObject);
+				temporaryOverlay2.duration = 0.7f;
+				temporaryOverlay2.animateShaderAlpha = true;
+				temporaryOverlay2.alphaCurve = AnimationCurve.EaseInOut(0f, 1f, 1f, 0f);
+				temporaryOverlay2.destroyComponentOnEnd = true;
+				temporaryOverlay2.originalMaterial = RoR2.LegacyResourcesAPI.Load<Material>("Materials/matHuntressFlashExpanded");
+				temporaryOverlay2.AddToCharacterModel(this.modelTransform.GetComponent<CharacterModel>());
+			}
+			this.modelTransform = base.GetModelTransform();
+			if (this.modelTransform)
+			{
+				this.characterModel = this.modelTransform.GetComponent<CharacterModel>();
+			}
+			if (this.characterModel)
+			{
+				this.characterModel.invisibilityCount++;
+			}
+			base.PlayCrossfade(this.enterAnimationLayerName, this.enterAnimationStateName, this.enterAnimationCrossfadeDuration);
+			base.characterDirection.forward = base.characterMotor.velocity.normalized;
+			if (NetworkServer.active)
+			{
+				//base.characterBody.AddBuff(RoR2Content.Buffs.HiddenInvincibility);
+			}
+			CreateBlinkEffect(Util.GetCorePosition(this.gameObject));
+		}
+		private void CreateBlinkEffect(Vector3 origin)
+		{
+			EffectData effectData = new EffectData();
+			effectData.rotation = Util.QuaternionSafeLookRotation(this.dashVector);
+			effectData.origin = origin;
+			EffectManager.SpawnEffect(MiniBlinkState.blinkPrefab, effectData, false);
+		}
+
+		public override void OnExit()
+		{
+			GameObject obj = base.outer.gameObject;
+			if (AltArtiPassive.instanceLookup.TryGetValue(obj, out var passive))
+			{
+				passive.SkillCast();
+			}
+
+			if (this.characterModel)
+			{
+				this.characterModel.invisibilityCount--;
+			}
+			if (NetworkServer.active)
+			{
+				//base.characterBody.RemoveBuff(RoR2Content.Buffs.HiddenInvincibility);
+			}
+			base.characterMotor.velocity *= this.speedCoefficientOnExit;
+			base.SmallHop(base.characterMotor, this.exitSmallHop);
+			Util.PlaySound(this.endSoundString, base.gameObject);
+			this.PlayAnimation(this.exitAnimationLayerName, this.exitAnimationStateName);
+			base.gameObject.layer = this.originalLayer;
+			base.characterMotor.Motor.RebuildCollidableLayers();
+			base.OnExit();
+		}
+
+		public override void PlayAnimation()
+		{
+			base.PlayAnimation();
+			base.PlayCrossfade(this.enterAnimationLayerName, this.enterAnimationStateName, this.enterAnimationCrossfadeDuration);
+		}
+
+		public override void AuthorityFixedUpdate()
+		{
+			base.AuthorityFixedUpdate();
+			if (!base.authorityInHitPause)
+			{
+				base.characterMotor.rootMotion += this.dashVelocity * base.GetDeltaTime();
+				base.characterDirection.forward = this.dashVelocity;
+				base.characterDirection.moveVector = this.dashVelocity;
+				base.characterBody.isSprinting = true;
+			}
+		}
+
+		public override void AuthorityModifyOverlapAttack(OverlapAttack overlapAttack)
+		{
+			base.AuthorityModifyOverlapAttack(overlapAttack);
+			overlapAttack.damage = _2ThunderstrikeSkill.damageCoefficient * this.damageStat;
+			overlapAttack.damageType = DamageTypeCombo.GenericSecondary;
+		}
+
+		public override void OnMeleeHitAuthority()
+		{
+			base.OnMeleeHitAuthority();
+			if (!this.hasIncreasedDuration)
+			{
+				this.hasIncreasedDuration = true;
+				this.duration *= _2ThunderstrikeSkill.durationMultOnHit;
+			}
+
+			float num = this.hitPauseDuration / this.attackSpeedStat;
+			if (this.selfOnHitOverlayEffectPrefab && num > 0.033333335f)
+			{
+				EffectData effectData = new EffectData
+				{
+					origin = base.transform.position,
+					genericFloat = this.hitPauseDuration / this.attackSpeedStat
+				};
+				effectData.SetNetworkedObjectReference(base.gameObject);
+				EffectManager.SpawnEffect(this.selfOnHitOverlayEffectPrefab, effectData, true);
+			}
+			foreach (HurtBox victimHurtBox in this.hitResults)
+			{
+				this.currentHitCount++;
+				float damageValue = base.characterBody.damage * this.delayedDamageCoefficient;
+				float num2 = this.delay + this.delayPerHit * (float)this.currentHitCount;
+				bool isCrit = base.RollCrit();
+				ThunderStrikeDash.HandleHit(base.gameObject, victimHurtBox, damageValue, this.delayedProcCoefficient, isCrit, num2, this.orbEffect, this.delayedEffectPrefab);
+			}
+		}
+
+		public override InterruptPriority GetMinimumInterruptPriority()
+		{
+			return InterruptPriority.PrioritySkill;
+		}
+
+		private static void HandleHit(GameObject attackerObject, HurtBox victimHurtBox, float damageValue, float procCoefficient, bool isCrit, float delay, GameObject orbEffectPrefab, GameObject orbImpactEffectPrefab)
+		{
+			if (!NetworkServer.active)
+			{
+				NetworkWriter networkWriter = new NetworkWriter();
+				networkWriter.StartMessage(21206);
+				networkWriter.Write(attackerObject);
+				networkWriter.Write(HurtBoxReference.FromHurtBox(victimHurtBox));
+				networkWriter.Write(damageValue);
+				networkWriter.Write(procCoefficient);
+				networkWriter.Write(isCrit);
+				networkWriter.Write(delay);
+				networkWriter.WriteEffectIndex(EffectCatalog.FindEffectIndexFromPrefab(orbEffectPrefab));
+				networkWriter.WriteEffectIndex(EffectCatalog.FindEffectIndexFromPrefab(orbImpactEffectPrefab));
+				networkWriter.FinishMessage();
+				NetworkConnection readyConnection = ClientScene.readyConnection;
+				if (readyConnection == null)
+				{
+					return;
+				}
+				readyConnection.SendWriter(networkWriter, QosChannelIndex.defaultReliable.intVal);
+				return;
+			}
+			else
+			{
+				if (!victimHurtBox || !victimHurtBox.healthComponent)
+				{
+					return;
+				}
+				SetStateOnHurt.SetStunOnObject(victimHurtBox.healthComponent.gameObject, delay);
+				OrbManager.instance.AddOrb(new SimpleLightningStrikeOrb
+				{
+					attacker = attackerObject,
+					target = victimHurtBox,
+					damageColorIndex = DamageColorIndex.Default,
+					damageValue = damageValue,
+					isCrit = isCrit,
+					procChainMask = default(ProcChainMask),
+					procCoefficient = procCoefficient
+				});
+				return;
+
+				OrbManager.instance.AddOrb(new DelayedHitOrb
+				{
+					attacker = attackerObject,
+					target = victimHurtBox,
+					damageColorIndex = DamageColorIndex.Default,
+					damageValue = damageValue,
+					damageType = new DamageTypeCombo(DamageType.Stun1s, DamageTypeExtended.Generic, DamageSource.Secondary),
+					isCrit = isCrit,
+					procChainMask = default(ProcChainMask),
+					procCoefficient = procCoefficient,
+					delay = delay,
+					orbEffect = orbEffectPrefab,
+					delayedEffectPrefab = orbImpactEffectPrefab
+				});
+				return;
+			}
+		}
+
+		[NetworkMessageHandler(msgType = 21206, client = false, server = true)]
+		private static void HandleReportMercFocusedAssaultHitReplaceMeLater(NetworkMessage netMsg)
+		{
+			GameObject attackerObject = netMsg.reader.ReadGameObject();
+			HurtBox victimHurtBox = netMsg.reader.ReadHurtBoxReference().ResolveHurtBox();
+			float damageValue = netMsg.reader.ReadSingle();
+			float procCoefficient = netMsg.reader.ReadSingle();
+			bool isCrit = netMsg.reader.ReadBoolean();
+			float num = netMsg.reader.ReadSingle();
+			EffectDef effectDef = EffectCatalog.GetEffectDef(netMsg.reader.ReadEffectIndex());
+			GameObject orbEffectPrefab = ((effectDef != null) ? effectDef.prefab : null) ?? null;
+			EffectDef effectDef2 = EffectCatalog.GetEffectDef(netMsg.reader.ReadEffectIndex());
+			GameObject orbImpactEffectPrefab = ((effectDef2 != null) ? effectDef2.prefab : null) ?? null;
+			ThunderStrikeDash.HandleHit(attackerObject, victimHurtBox, damageValue, procCoefficient, isCrit, num, orbEffectPrefab, orbImpactEffectPrefab);
+		}
+	}
+}
---
+++ AltArtificerExtended/Skills/2Secondary/2ThunderstrikeSkill.cs
@@ -0,0 +1,98 @@
+﻿using ArtificerExtended.States;
+using ArtificerExtended.Unlocks;
+using BepInEx.Configuration;
+using EntityStates;
+using R2API;
+using RoR2;
+using RoR2.Projectile;
+using RoR2.Skills;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace ArtificerExtended.Skills
+{
+    class _2ThunderstrikeSkill : SkillBase
+    {
+        public static GameObject shockwaveZapConePrefab;
+        public static int shockwaveMaxAngleFilter = 35;
+        public static float shockwaveMaxRange = 20;
+
+
+        public static float baseDuration = 0.2f;
+        public static float durationMultOnHit = 1.5f;
+        public static GameObject lightningOrbEffect;
+        public static GameObject lightningImpactEffect;
+        public static Material enterOverlayMaterial;
+        public static float speedCoefficient = 12;
+        public static float damageCoefficient = 1;
+        public static float procCoefficient = 0.25f;
+        public static float delayDamageCoefficient = 9;
+        public static float delayProcCoefficient = 1;
+        public override string SkillName => "Thunderstrike";
+
+        public override string SkillDescription => $"<style=cIsDamage>Stunning</style>. " +
+            $"Become a beam of energy, surging a short distance forward. " +
+            $"Enemies struck will attract lightning for <style=cIsDamage>{Tools.ConvertDecimal(delayDamageCoefficient)} damage</style>.";
+
+        public override string SkillLangTokenName => "THUNDERDASH";
+
+        public override UnlockableDef UnlockDef => GetUnlockDef(typeof(OverkillOverloadingUnlock));
+
+        public override string IconName => "shockwaveicon";
+
+        public override MageElement Element => MageElement.Lightning;
+
+        public override Type ActivationState => typeof(ThunderStrikeDash);
+
+        public override SkillFamily SkillSlot => ArtificerExtendedPlugin.mageSecondary;
+
+        public override SimpleSkillData SkillData => new SimpleSkillData
+            (
+                baseRechargeInterval: 5,
+                interruptPriority: InterruptPriority.Skill,
+                mustKeyPress: true
+            );
+
+        public override void Hooks()
+        {
+        }
+
+        public override void Init(ConfigFile config)
+        {
+            KeywordTokens = new string[1] { "KEYWORD_STUNNING" };
+
+            lightningOrbEffect = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/LightningStrikeOnHit/SimpleLightningStrikeOrbEffect.prefab").WaitForCompletion();
+            lightningImpactEffect = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/LightningStrikeOnHit/SimpleLightningStrikeImpact.prefab").WaitForCompletion();
+
+            CreateLang();
+            CreateSkill();
+        }
+
+        private void RegisterProjectileShockwave()
+        {
+            shockwaveZapConePrefab = RoR2.LegacyResourcesAPI.Load<GameObject>("Prefabs/Projectiles/LoaderZapCone").InstantiateClone("shockwaveZapCone", true);
+
+            ProjectileProximityBeamController ppbc = shockwaveZapConePrefab.GetComponent<ProjectileProximityBeamController>();
+            ppbc.attackRange = shockwaveMaxRange - 5;
+            ppbc.maxAngleFilter = shockwaveMaxAngleFilter;
+            ppbc.damageCoefficient = 0; // 10;
+            ppbc.procCoefficient = 1;
+            ppbc.attackInterval = 0.15f;
+
+            ShakeEmitter shake = shockwaveZapConePrefab.AddComponent<ShakeEmitter>();
+            shake.radius = 80; //40
+            shake.duration = 0.3f; //0.2f
+            shake.shakeOnEnable = false;
+            shake.shakeOnStart = true;
+            shake.amplitudeTimeDecay = true;
+            shake.scaleShakeRadiusWithLocalScale = false;
+
+            shockwaveZapConePrefab.GetComponent<DestroyOnTimer>().duration = 2f;
+
+            ContentPacks.projectilePrefabs.Add(shockwaveZapConePrefab);
+        }
+    }
+}
