ed9d74479969b56e776aeab4877728ac1eda6934
BORBO <bwofodol@gmail.com>
2025-09-10 13:04:21 -0500

    increased ice surge height

--- AltArtificerExtended/Skills/3Utility/3IceSurgeSkill.cs
+++ AltArtificerExtended/Skills/3Utility/3IceSurgeSkill.cs
@@ -15,7 +15,7 @@ namespace ArtificerExtended.Skills
         public static float blastDamageCoefficient = 3.5f;
         public static float wallDamageCoefficient = 1.2f;
         public static float baseDurationHorizontal = 0.8f;
-        public static float baseDurationVertical = 1.2f;
+        public static float baseDurationVertical = 1.5f;
 
         public override string TOKEN_IDENTIFIER => "ICESURGE";
         public override string SkillName => "Cryoburst";
